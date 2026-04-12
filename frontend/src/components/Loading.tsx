83c95d396e202234b2434c2109bc8e5583a783e4
lanceu0128 <lanceu0128@gmail.com>
2026-04-12 16:55:11 -0700

    add terra loading screen

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,12 +1,84 @@
+import { useEffect, useState } from "react";
+
+const MESSAGES = [
+  "Accomplishing",
+  "Actioning",
+  "Actualizing",
+  "Baking",
+  "Brewing",
+  "Calculating",
+  "Cerebrating",
+  "Churning",
+  "Terraing",
+  "Coalescing",
+  "Cogitating",
+  "Computing",
+  "Conjuring",
+  "Considering",
+  "Cooking",
+  "Crafting",
+  "Creating",
+  "Crunching",
+  "Deliberating",
+  "Determining",
+  "Doing",
+  "Effecting",
+  "Finagling",
+  "Forging",
+  "Forming",
+  "Generating",
+  "Hatching",
+  "Herding",
+  "Honking",
+  "Hustling",
+  "Ideating",
+  "Inferring",
+  "Manifesting",
+  "Marinating",
+  "Moseying",
+  "Mulling",
+  "Mustering",
+  "Musing",
+  "Noodling",
+  "Percolating",
+  "Pondering",
+  "Processing",
+  "Puttering",
+  "Reticulating",
+  "Ruminating",
+  "Schlepping",
+  "Shucking",
+  "Simmering",
+  "Smooshing",
+  "Spinning",
+  "Stewing",
+  "Synthesizing",
+  "Thinking",
+  "Transmuting",
+  "Vibing",
+  "Working",
+];
+
 export default function Loading() {
+  const [dots, setDots] = useState(1);
+  const [message] = useState(() => MESSAGES[Math.floor(Math.random() * MESSAGES.length)]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prevDots) => (prevDots % 3) + 1);
+    }, 750);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="min-h-96 w-full p-4 grow flex gap-3 flex-col items-center justify-center">
       <img
-        className="w-full max-w-72 animate-pulse"
-        src="/h4i_umd_logo.png"
-        alt="hack4impact-UMD"
+        className="w-full max-w-54"
+        src="/terra.png"
+        alt="Terra Image"
       />
-      <p className="text-center">Doing some important stuff, hold on...</p>
+      <p className="text-center">{message}{".".repeat(dots)}</p>
     </div>
   );
 }
