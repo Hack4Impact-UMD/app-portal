d4cf0fbc9e82b870c7ffd4a372445a85c669cf6e
Lance <lanceu0128@gmail.com>
2026-05-10 17:17:44 -0700

    initial set-up for monorepo; shared package build working

---
+++ shared/src/index.ts
@@ -0,0 +1,10 @@
+export enum ApplicantRole {
+  Bootcamp = "bootcamp",
+  Engineer = "engineer",
+  Designer = "designer",
+  Product = "product",
+  // Sourcing = "sourcing",
+  TechLead = "tech-lead",
+  SocialMedia = "social-media-manager",
+  OutreachCoord = "outreach-coordinator",
+}
