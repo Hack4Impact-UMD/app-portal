d920d1bd291c88547991072918caedc982eb7f1c
Lance <lanceu0128@gmail.com>
2026-05-30 16:12:00 -0700

    fix application form type mismatch + anti-pattern

--- backend/functions/src/models/appForm.ts
+++ backend/functions/src/models/appForm.ts
@@ -1,14 +1,11 @@
 import { ApplicationFormBaseSchema } from "@app-portal/shared/types";
-import type { ApplicationFormBase } from "@app-portal/shared/types";
 import { Timestamp } from "firebase-admin/firestore";
 import { z } from "zod";
 
 // ⚠️ do not edit this type!!!
 // edit the ApplicationFormBaseSchema in shared instead!!!
-export const ApplicationFormSchema = ApplicationFormBaseSchema.extend({
-  dateSubmitted: z.custom<Timestamp>((d) => d instanceof Timestamp),
+const ApplicationFormSchema = ApplicationFormBaseSchema.extend({
+  dueDate: z.custom<Timestamp>((d) => d instanceof Timestamp),
 });
 
-export interface ApplicationForm extends ApplicationFormBase {
-  dueDate: Timestamp;
-}
+export type ApplicationForm = z.infer<typeof ApplicationFormSchema>;
